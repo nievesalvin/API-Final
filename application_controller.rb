bc9f175e6a27c448c8e273fdf28848024671464f
dakaraia <dakaraia6799@hstat.org>
2018-03-12 16:02:39 +0000

    revised index

--- application_controller.rb
+++ application_controller.rb
@@ -10,13 +10,8 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
-  
-<<<<<<< HEAD
   get 'weather' do
     erb :index
   end
-=======
-  post '/result' do
-  ende
->>>>>>> d38eed7e62c0765f020e3245dd776e17734da04a
 end
+
