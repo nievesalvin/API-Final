08283d8add48da803c03323268c4612c9c8faf58
dakaraia <dakaraia6799@hstat.org>
2018-03-16 15:17:35 +0000

    atempt to get api to work

--- application_controller.rb
+++ application_controller.rb
@@ -18,10 +18,10 @@ class ApplicationController < Sinatra::Base
   
   post '/result' do
     puts params
-    @city = params[:city] #appears in the terminal
-    @country = params[:country]#^same thing. 
-    @weather_result = weather(city) #sets weather_result equal to the definition which then goes and send the weather to the the result  
-    
+    city = params[:city] #appears in the terminal
+    country = params[:country]#^same thing. 
+    @weather_result = Weather.new(@city) #sets weather_result equal to the definition which then goes and send the weather to the the result  
+    @weather_result.find_weather
     erb :result
   end
   
--- models/model.rb
+++ models/model.rb
@@ -1,17 +1,18 @@
 # require "json"
 # require "http"
-# require "optparse"
+require "optparse"
 # require "pp"
-# require "net/http"
-API_KEY = ENV["bbbbce6a9cd9956ed1161c064ca27897"] 
+#require "net/http"
+API_KEY = ENV["API_KEY"] 
 #get the weather
 class Weather
     # takes in an input from the user, which is then used as the city that the API searches for
+    attr_reader :city
    def initialize(city)
        @city = :city
    end
    #this instance is supposed use the API key and the url so that it could go into the api and retrieve the city which is already saved as the user input
-    def weather
+    def find_weather
     url = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={#{API_KEY})"
     uri = URI(url)
     response = Net::HTTP.get(uri)
